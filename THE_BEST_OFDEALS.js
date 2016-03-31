939c74c4a18d543adf0f51d17e57782ed7dc0d0b
likhona <likhonapr@projectcodex.co>
2016-03-31 18:07:51 +0200

    latest

--- THE_BEST_OFDEALS.js
+++ THE_BEST_OFDEALS.js
@@ -1,16 +1,16 @@
-var avoString = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+var avoString = "1 for R3,2 for R7,3 for R10,5 for R14.50.";
+
 avoDivide = [];
 var splitpric = avoString
-.replace("for ", "for:")
-.replace("for ", "for:")
-.replace("for ", "for:")
-.replace("for ", "for:")
- var avoDivide = splitpric.split(",");
+.replace("for ", ":").replace("for ", ":")
+.replace("for ", ":").replace("for ", ":")
+.replace("R", " ").replace("R", " ")
+.replace("R", " ").replace("R", " ")
+//console.log(avoString)
+var avoDivide = splitpric.split(",");
+//console.log(avoDivide);
+// // console.log(avoDivide);
+ avoDivide.forEach(function(item){
+ console.log(item);
 
-console.log(avoDivide);
-
-avoDivide.forEach(function(item, index){
-console.log(item, index);
-
-
-});
+ });
