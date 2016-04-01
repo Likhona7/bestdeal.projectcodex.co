543dde1aa26e5c2d66808e97b9a081e7798f629d
likhona <likhonapr@projectcodex.co>
2016-04-01 18:32:27 +0200

    latest

--- THE_BEST_OFDEALS.js
+++ THE_BEST_OFDEALS.js
@@ -2,6 +2,7 @@ var avoString = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
 
 avoDivide = [];
 newAvoDivide = [];
+pricePerAvo = [];
 var splitpric = avoString.replace(/ for R/g," ");
 var avoDivide = splitpric.split(", ");
 avoDivide.forEach(function(avoString) {
@@ -10,8 +11,13 @@ newAvoDivide.push(avoString.split(" "));
 newAvoDivide.forEach(function(item){
 var price = item[1]
 var quantity = item[0]
-var pricePerAvo = Number((price / quantity).toFixed(2));
+pricePerAvo.push(Number((price / quantity).toFixed(2)));
 
-
-console.log(pricePerAvo);
 });
+var min = Math.min.apply(null, pricePerAvo),
+    max = Math.max.apply(null, pricePerAvo);
+
+
+    console.log(pricePerAvo);
+    console.log(min,"Cheapest deal");
+    console.log(max,"Most expensive deal");
