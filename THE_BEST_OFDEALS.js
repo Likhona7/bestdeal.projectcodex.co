120dbbe6c2e5c03e31cc605062fc8537b86c8b5d
likhona <likhonapr@projectcodex.co>
2016-04-01 17:01:40 +0200

    latest

--- THE_BEST_OFDEALS.js
+++ THE_BEST_OFDEALS.js
@@ -4,15 +4,14 @@ avoDivide = [];
 newAvoDivide = [];
 var splitpric = avoString.replace(/ for R/g," ");
 var avoDivide = splitpric.split(", ");
-
 avoDivide.forEach(function(avoString) {
 newAvoDivide.push(avoString.split(" "));
  });
-
 newAvoDivide.forEach(function(item){
 var price = item[1]
 var quantity = item[0]
+var pricePerAvo = Number((price / quantity).toFixed(2));
 
-var ans = price / quantity;
-console.log(ans);
+
+console.log(pricePerAvo);
 });
